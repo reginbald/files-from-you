9e5e9e17de08012748affa5feced82b89ee93f8f
reginbald <reginbald@users.noreply.github.com>
2018-03-15 18:30:30 +0100

    Displaying graph for selected transfer

--- src/app/transfers/transfer-details/transfer-details.component.ts
+++ src/app/transfers/transfer-details/transfer-details.component.ts
@@ -1,5 +1,7 @@
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { ChartComponent } from 'angular2-chartjs';
+
 import { Transfer } from './../transfer';
-import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-transfer-details',
@@ -7,11 +9,20 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./transfer-details.component.css'],
 })
 export class TransferDetailsComponent implements OnInit {
+  title: string = 'File: ';
   chartColors: String[] = ['#3498db', '#e74c3c', '#2ecc71', '#f1c40f'];
+  @ViewChild(ChartComponent) transferChart: ChartComponent;
 
   @Input()
   set transfer(transfer: Transfer) {
-    console.log('Transfer', transfer);
+    if (transfer === undefined) return;
+    this.title = 'File: ' + transfer.fileName;
+    this.data.datasets[0].label = transfer.usage[0].client;
+    this.data.datasets[0].data = transfer.usage[0].usage;
+    this.data.datasets[1].label = transfer.usage[1].client;
+    this.data.datasets[1].data = transfer.usage[1].usage;
+
+    this.transferChart.chart.update();
   }
 
   type = 'line';
@@ -19,15 +30,15 @@ export class TransferDetailsComponent implements OnInit {
     labels: ['0%', '10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'],
     datasets: [
       {
-        label: 'Client 1',
-        data: [0, 10, 100, 100, 100, 80, 100, 100, 70, 0],
+        label: '',
+        data: [],
         backgroundColor: this.chartColors[0],
         borderColor: this.chartColors[0],
         fill: false,
       },
       {
-        label: 'Client 2',
-        data: [0, 10, 80, 40, 30, 80, 100, 100, 40, 0],
+        label: '',
+        data: [],
         backgroundColor: this.chartColors[1],
         borderColor: this.chartColors[1],
         fill: false,
@@ -37,10 +48,6 @@ export class TransferDetailsComponent implements OnInit {
   options = {
     responsive: true,
     maintainAspectRatio: false,
-    title: {
-      display: true,
-      text: 'Filename: large-file.exe',
-    },
     scales: {
       xAxes: [
         {
